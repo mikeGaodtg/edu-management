ea0d94dbb9065a1f3258dd925b02845850595488
MikeGao <mikegao772@gmail.com>
2022-06-07 01:06:40 +1200

    haha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Switch,Route,Redirect,BrowserRouter} from 'react-router-dom';
+import Login from './views/Login/Index';
+import Layout from './views/Layout/Index';
+import { authLogin } from './utils/auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  render() {
+
+
+    return (
+      <div>
+         <BrowserRouter>
+         <Switch>
+         <Route path="/" exact render={(props)=>{
+           return <Redirect to='/index'></Redirect>
+        }}  ></Route>
+         <Route path='/index' render={(props)=>{
+           if(authLogin()){
+            return <Layout {...props}></Layout>
+           }return <Redirect to='/login' ></Redirect>
+        
+        }} ></Route>
+         <Route path='/login'  render={(props)=>{
+           if(!authLogin()){
+            return <Login {...props}></Login>
+           }return <Redirect to='/index/home' ></Redirect>
+        
+        }} ></Route>
+         </Switch>
+      
+         </BrowserRouter>
+      </div>
+    )
+  }
 }
-
-export default App;
